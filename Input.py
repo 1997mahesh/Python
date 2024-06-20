ede4d3de5ea34198934d4281018f3a041d628e2f
1997mahesh <officeman1997@gmail.com>
2024-06-20 07:34:21 +0530

    More practice on Input function

--- Input.py
+++ Input.py
@@ -1,12 +1,38 @@
-# name = input('Enter youre name:- ')
-# print('Welcome ', name)
 
+#  1
+'''
+name = input('Enter youre name:- ')
+print('Welcome ', name)
+'''
 
-
+#  2
+'''
+# Addition of two number 
 a = int(input('enter the first number:- '))
 b = int(input('enter the second number:- '))
 sum = a+b
+# print('The sum of A & B is:- ', sum)
+'''
 
-print('The sum of A & B is:- ', sum)
+#  3 
+'''
+# Area of a square 
+s = int(input('Enter the one side of the square:-  '))
+print('The Area of the square is:- ', s*s)
+'''
 
+#  4
+'''
+# Average of two number
+a = float(input('Enter first number:- '))
+b = float(input('Enter second number:- '))
+average = (a+b)/2
+print('The average of A & B is:- ', average)
+'''
 
+#  5 
+
+# Check A>=B Or Not
+a = int(input('enter the first number:- '))
+b = int(input('enter the second number:- '))
+print(a>=b)
